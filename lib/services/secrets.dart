a3e6120c67c3ef78aa89e5cdbf3f2c7be25143ac
andy <66240551+flandolf@users.noreply.github.com>
2024-01-29 21:04:06 +1100

    hehe

--- lib/services/secrets.dart
+++ lib/services/secrets.dart
@@ -1,2 +1,2 @@
-const String clientId = "686a7f6dc7e94ebea052bcb2ef3d5c59";
-const String clientSecret = "ee1c0cfa492f46fd9d06e5743de63be8";
+const String clientId = "YOUR_CLIENT_ID";
+const String clientSecret = "CLIENT_SECRET";
